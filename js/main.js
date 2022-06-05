9ac13f082f69c31f193ad26c9996356014e1e3da
piyu010 <piyush.sharma@stud.noroff.no>
2022-06-05 20:22:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -129,14 +129,10 @@ function updatetotal() {
 }
 
 
+// navbar for mobile
 
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-icon').onclick = () => {
   navbar.classList.toggle('active');
-  search.classList.remove('active');
-  cart.classList.remove('active');
-  user.classList.remove('active');
-
-
 }
